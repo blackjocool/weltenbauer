5a635139367b90f9cda180763b4d866e7cd8d75f
blackjocool <93588576+blackjocool@users.noreply.github.com>
2021-11-02 15:17:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello!')
+var apiUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+var callLink = async function (url) {
+  var response = await fetch(url);
+
+  return response.json();
+};
+
+var getData = async function (limit) {
+  return callLink(apiUrl + '?limit=' + limit);
+};
+
+getData(10).then(async function (pokemon) {
+  console.log(pokemon.results);
+});
